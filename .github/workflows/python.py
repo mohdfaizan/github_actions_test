c24256759b4cc9acd486288bb15d307fd2eaa11b
mohdfaizan <mfumar6@gmail.com>
2024-11-28 10:39:46 +0530

    Create python.py
    Added a python code to check flake 8

---
+++ .github/workflows/python.py
@@ -0,0 +1,13 @@
+# sample.py
+
+def hello_world():
+    print("Hello World") # This is a valid line
+    
+def example_function():
+ unused_var = 10  # IndentationError: inconsistent use of tabs and spaces in indentation
+    print("This function is not doing anything") # This line has wrong indentation
+    
+hello_world()
+
+if x== 5:   # E701: multiple statements on one line
+ print("The value of x is", x) # Undefined variable `x`
